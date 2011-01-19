db0ffd2917528dea73c58c13fb052f02d62a8b0e
Christian Pfeffer Gjengedal <cpg@ez.no>
2011-01-19 16:31:13 +0100

    symlink update

--- ezext.sh
+++ ezext.sh
@@ -3,20 +3,21 @@
 # Script adding symlinks to extensions 
 pushd /home/cpg/workspace/ezpublish/extension
 rm -fr ezflow ezwt ezwebin ezstarrating
+
+ln -s ../../ezcomments ezcomments
 ln -s ../../ezevent ezevent
 ln -s ../../ezfind ezfind
 ln -s ../../ezflow/packages/ezflow_extension/ezextension/ezflow ezflow
 ln -s ../../ezie ezie 
 ln -s ../../ezjscore ezjscore
 ln -s ../../ezmultiupload ezmultiupload 
-ln -s ../../ezoe ezoe
 ln -s ../../ezodf ezodf
+ln -s ../../ezoe ezoe
+ln -s ../../ezstarrating/packages/ezstarrating_extension/ezextension/ezstarrating ezstarrating
+ln -s ../../ezstyleeditor ezstyleeditor
+ln -s ../../ezsurvey ezsurvey
 ln -s ../../ezwebin/packages/ezwebin_extension/ezextension/ezwebin ezwebin
 ln -s ../../ezwt/packages/ezwt_extension/ezextension/ezwt ezwt
-ln -s ../../ezcomments ezcomments
-ln -s ../../ezstarrating/packages/ezstarrating_extension/ezextension/ezstarrating ezstarrating
-ln -s ../../ezsurvey ezsurvey
-ln -s ../../ezmbpaex ezmbpaex
-ln -s ../../ezstyleeditor ezstyleeditor
+
 popd
 
