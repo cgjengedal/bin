0109efad19e1f5b18d2eedd8360237dd877680c9
Christian Pfeffer Gjengedal <cpg@ez.no>
2011-03-25 08:51:14 +0100

    Additional aliases

--- customprofile.sh
+++ customprofile.sh
@@ -13,8 +13,10 @@ PATH=$PATH:/home/cpg/bin
 alias clamscan='freshclam; clamscan / -r --quiet --exclude-dir=^/sys\|^/proc\|^/dev'
 alias up='yum update -y'
 alias ll='ls -lh --color=auto'
-alias tplfiles='find -L . -name "*.tpl"'
-alias phpfiles='find -L . -name "*.php"'
-alias jsfiles='find -L . -name "*.js"'
+alias cssfiles='find -L . -name "*.css"'
 alias inifiles='find -L . -name "*.ini*"'
+alias jsfiles='find -L . -name "*.js"'
+alias phpfiles='find -L . -name "*.php"'
+alias sqlfiles='find -L . -name "*.sql"'
+alias tplfiles='find -L . -name "*.tpl"'
 
