6ce25ab72f6a3fb190c5b132dc2387946a84f9c7
Christian Pfeffer Gjengedal <christian.gjengedal@gmail.com>
2015-08-04 13:41:16 +0200

    Old stuff...

--- ezccache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Script refreshing the svn checked out eZ setup
-pushd /home/cpg/workspace/ezpublish
-rm -fr var/ezwebin_site/cache/*; rm -fr var/ezflow_site/cache/*
-popd
-
--- launcheclipse.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export GDK_NATIVE_WINDOWS=true
-/home/cpg/eclipse/eclipse
-
